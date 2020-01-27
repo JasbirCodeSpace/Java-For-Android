5db99ec3e0c6dc8db8dad66875121a76e7d4b980
Jasbir <shikhawat.jasbir@gmail.com>
2020-01-27 21:52:06 +0530

    Add files via upload

---
+++ Week4/calculator/app/src/main/java/mooc/vandy/java4android/calculator/logic/Subtract.java
@@ -0,0 +1,22 @@
+package mooc.vandy.java4android.calculator.logic;
+
+/**
+ * Perform the Subtract operation.
+ */
+public class Subtract {
+    private int argument1 = 0;
+    private int argument2 = 0;
+
+    // constructor to initialize instance variables
+    public Subtract(int argument1,int argument2){
+    	this.argument1 = argument1;
+    	this.argument2 = argument2;
+    }
+
+    // overridden toString() method to return subtraction result
+    @Override
+    public String toString(){
+    	int result = this.argument1 - this.argument2;
+    	return String.valueOf(result);
+    }
+}
